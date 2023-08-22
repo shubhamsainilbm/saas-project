9c672cff7437b57cf39aca97ee0a442cd39ecfd5
shubhamsainilbm <139846344+shubhamsainilbm@users.noreply.github.com>
2023-08-22 19:54:04 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -13,6 +13,7 @@ const allowedOrigins = [
   "http://192.168.11.59:5174",
   "http://192.168.11.59:5173",
   "http://127.0.0.1:5173",
+  "https://csaas.lbmdemo.com"
 ];
 
 export default allowedOrigins;
